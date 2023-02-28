4e140e3865d5e4071131d9bf313fbdf7defd61cb
Jenny <ntnhan5@cmcglobal.vn>
2023-02-28 22:43:03 +0700

    add style option

--- src/App.js
+++ src/App.js
@@ -6,11 +6,20 @@ function App() {
     const [numRows, setNumRows] = React.useState(1);
     const [numCols, setNumCols] = React.useState(1);
     const [squareSize, setSquareSize] = React.useState(100);
+    const [devideStyle, setDevideStyle] = React.useState("all")
 
     const handleResize = (index, size) => {
         setSquareSize(size.width / numCols);
     };
-
+    const renderClass = () => {
+        let className = "all"
+        if(devideStyle === "kengang") {
+            className = "square-ke-ngang"
+        }else{
+            className = "square-full"
+        }
+        return className
+    }
     const renderSquare = (rowIndex, colIndex) => {
         return (
             <ResizableBox
@@ -21,7 +30,7 @@ function App() {
                 resizeHandles={['se']}
                 minConstraints={[squareSize, squareSize]}
                 maxConstraints={[squareSize, squareSize]}
-                className="square"
+                className={renderClass()}
             >
                 {/*{rowIndex * numCols + colIndex + 1}*/}
             </ResizableBox>
@@ -45,15 +54,21 @@ function App() {
             <div className="group-button">
                 <div>
                     <label>Rows:</label>
-                    <input type="number" value={numRows} onChange={(event) => setNumRows(event.target.value !== '' ? Number(event.target.value) : 1)} />
+                    <input type="text" value={numRows} onChange={(event) => setNumRows(Number(event.target.value))} />
                 </div>
                 <div>
                     <label>Columns:</label>
-                    <input type="number" value={numCols} onChange={(event) => setNumCols(event.target.value !== '' ? Number(event.target.value) : 1)} />
+                    <input type="number" value={numCols} onChange={(event) => setNumCols(Number(event.target.value))} />
                 </div>
                 <div>
                     <label>Square size:</label>
-                    <input type="number" value={squareSize} onChange={(event) => setSquareSize(event.target.value !== '' ? Number(event.target.value) : 20)} />
+                    <input type="text" value={squareSize} onChange={(event) => setSquareSize(Number(event.target.value))} />
+                </div>
+                <div>
+                    <select onChange={(event) => setDevideStyle(event.target.value)}>
+                        <option value="all">All</option>
+                        <option value="kengang">Kẻ ngang</option>
+                    </select>
                 </div>
                 <div className="print-button">
                     <button onClick={() => window.print()}>Print</button>
