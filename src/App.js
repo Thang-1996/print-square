3c2d4df19f4bf95e911d29af46da4d375e2d6e1e
Jenny <ntnhan5@cmcglobal.vn>
2023-02-28 21:49:58 +0700

    finish code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
+import { ResizableBox } from 'react-resizable';
+import React from "react"
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [numRows, setNumRows] = React.useState(2);
+    const [numCols, setNumCols] = React.useState(2);
+    const [squareSize, setSquareSize] = React.useState(100);
+
+    const handleResize = (index, size) => {
+        setSquareSize(size.width / numCols);
+    };
+
+    const renderSquare = (rowIndex, colIndex) => {
+        return (
+            <ResizableBox
+                key={`${rowIndex}-${colIndex}`}
+                width={squareSize}
+                height={squareSize}
+                onResize={(event, { size }) => handleResize(rowIndex * numCols + colIndex, size)}
+                resizeHandles={['se']}
+                minConstraints={[squareSize, squareSize]}
+                maxConstraints={[squareSize, squareSize]}
+                className="square"
+            >
+                {/*{rowIndex * numCols + colIndex + 1}*/}
+            </ResizableBox>
+        );
+    };
+
+    const renderGrid = () => {
+        const squares = [];
+        for (let i = 0; i < numRows; i++) {
+            const rowSquares = [];
+            for (let j = 0; j < numCols; j++) {
+                rowSquares.push(renderSquare(i, j));
+            }
+            squares.push(<div key={`row-${i}`} className="row">{rowSquares}</div>);
+        }
+        return <div className="grid">{squares}</div>;
+    };
+
+    return (
+        <div className="App">
+            <div className="group-button">
+                <div>
+                    <label>Rows:</label>
+                    <input type="number" value={numRows} onChange={(event) => setNumRows(Number(event.target.value))} />
+                </div>
+                <div>
+                    <label>Columns:</label>
+                    <input type="number" value={numCols} onChange={(event) => setNumCols(Number(event.target.value))} />
+                </div>
+                <div>
+                    <label>Square size:</label>
+                    <input type="text" value={squareSize} onChange={(event) => setSquareSize(Number(event.target.value))} />
+                </div>
+                <div className="print-button">
+                    <button onClick={() => window.print()}>Print</button>
+                </div>
+            </div>
+
+            {renderGrid()}
+        </div>
+    );
 }
 
 export default App;
